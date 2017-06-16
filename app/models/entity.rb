210e7d1a5e351a0c2a25919b99780caddb4689fa
Phong Pham <phizzy@gmail.com>
2017-06-16 15:29:18 -0400

    Add methods to Entity model to show whether utility present

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,3 +1,18 @@
 class Entity < ActiveRecord::Base
   #serialize :config, JSON
+  def has_config
+    self.has_attribute? 'config'
+  end
+
+  def has_water
+    self[:config]["features"]["utilities"]["water"] == true
+  end
+
+  def has_gas
+    self[:config]["features"]["utilities"]["gas"] == true
+  end
+
+  def has_electric
+    self[:config]["features"]["utilities"]["electric"] == true
+  end
 end
